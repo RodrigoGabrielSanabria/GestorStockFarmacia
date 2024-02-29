9b0dcfce2d0b867a1378384375b1996a6def6bf3
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-02-29 15:30:48 -0300

    Create Venta.cs

---
+++ Dominio/Venta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dominio
+{
+    internal class Venta
+    {
+        public int ID { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal TotalVenta { get; set; }
+        public List<DetalleVenta> DetallesVenta { get; set; }
+
+        public Venta()
+        {
+            DetallesVenta = new List<DetalleVenta>();
+        }
+
+    }
+}
