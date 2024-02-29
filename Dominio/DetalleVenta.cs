46bdb39b86dd157e5c7908e3a7295bcc1c2c8860
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-02-29 15:31:03 -0300

    Create DetalleVenta.cs

---
+++ Dominio/DetalleVenta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class DetalleVenta
+    {
+        public int ID { get; set; }
+        public int IDVenta { get; set; }
+        public int IDMedicamento { get; set; }
+        public int CantidadVendida { get; set; }
+        public decimal PrecioTotal { get; set; }
+    }
+}
