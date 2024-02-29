60a0673288f4bff24cd53b45f234db9c0c9b9db5
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-02-29 15:31:00 -0300

    Create Medicamento.cs

---
+++ Dominio/Medicamento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Medicamento
+    {
+
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public double  precioUnitario{ get; set; }
+        public int cantidadStock { get; set; }
+
+    }
+}
