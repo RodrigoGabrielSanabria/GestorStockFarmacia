76d0a770efc96e2d4e4e90c31b6f23d24d876664
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-03-04 09:23:32 -0300

    Create Main.cs

---
+++ Farmacia/Main.cs
@@ -0,0 +1,26 @@
+﻿using Dominio;
+using Conexion_DB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Farmacia
+{
+    public partial class FormMedicamentos : Form
+    {
+        public FormMedicamentos()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        
+    }
+}
