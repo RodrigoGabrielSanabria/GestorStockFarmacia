d94020edcd9a4871b7c1b7b967f1a07350c55168
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2024-03-04 15:30:20 -0300

    Update Medicamento.cs

--- Dominio/Medicamento.cs
+++ Dominio/Medicamento.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Medicamento
+    public class Medicamento
     {
 
         public int id { get; set; }
         public string nombre { get; set; }
         public double  precioUnitario{ get; set; }
         public int cantidadStock { get; set; }
+
+        public double precioTotal { get; set; }
 
     }
 }
